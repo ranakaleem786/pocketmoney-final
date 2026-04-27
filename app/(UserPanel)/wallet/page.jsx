d70cc1805ec1aed3eb80779af97689770bb11353
ranakaleem786 <ranakaleem786422@gmail.com>
2026-04-27 23:41:56 +0500

    d

--- app/(UserPanel)/wallet/page.jsx
+++ app/(UserPanel)/wallet/page.jsx
@@ -1,21 +1,23 @@
+
 import AppBar from '@/components/AppBar'
 import BottomNav from '@/components/BottomNav'
 import AddPaymentMethod from '@/components/wallet/AddPaymentMethod'
 import SummaryCard from '@/components/wallet/SummaryCard'
 import WithdrawalBox from '@/components/wallet/WithdrawalBox'
 import WithdrawalHistory from '@/components/wallet/WithdrawalHistory'
+import { api } from '@/lib/api'
 import { serverApi } from '@/lib/serverApi'
 
 import React from 'react'
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 
 const page = async () => {
   
-  const rewardRes = await serverApi("/bounty/get-claimed-reward-count");
-  const getPaymentRes = await serverApi("/user/get-payment-method");
-  const getWithdrawals = await serverApi("/user/get-withdrawals");
+  // const rewardRes = await serverApi("/bounty/get-claimed-reward-count");
+  const getPaymentRes = await api("/user/get-payment-method");
+  // const getWithdrawals = await serverApi("/user/get-withdrawals");
 
   console.log(getPaymentRes?.data);
   
